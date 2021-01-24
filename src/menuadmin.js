d5b33d50e4adc3aeeab264f45619fe6b2c4e6d33
ToinNetuh <gpantonioneto@gmail.com>
2021-01-24 17:44:47 -0300

    Commiter

---
+++ src/menuadmin.js
@@ -0,0 +1,29 @@
+const menuadmin = (prefix, pushname) => {
+ return `oiin aqui e o menu dos admins
+ _obs para o bot executar esses comandos e nescessario dar adm para o bot_
+ 
+ ◪ *COMANDO DOS ADMS*
+ │
+ ├─ ❏ ${prefix}opengc
+ ├─ ❏ ${prefix}closegc
+ ├─ ❏ ${prefix}promote
+ ├─ ❏ ${prefix}demote
+ ├─ ❏ ${prefix}tagall
+ ├─ ❏ ${prefix}add
+ ├─ ❏ ${prefix}kick
+ ├─ ❏ ${prefix}listadmins
+ ├─ ❏ ${prefix}linkgroup
+ ├─ ❏ ${prefix}leave
+ ├─ ❏ ${prefix}welcome
+ ├─ ❏ ${prefix}nsfw
+ ├─ ❏ ${prefix}leveling
+ ├─ ❏ ${prefix}level
+ ├─ ❏ ${prefix}delete
+ ├─ ❏ ${prefix}simih
+ └─ ❏ ${prefix}ownergroup
+ `
+
+
+}
+
+exports.menuadmin = menuadmin
